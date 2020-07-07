8d2b9f141fe364e4dcbebf948967541953022cf6
Asim Naseer <67946028+GetAsim@users.noreply.github.com>
2020-07-07 18:35:50 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import MainGrid from './components/MainGrid';
+import GlobalData from './components/GlobalData';
+import Header from './components/Header';
+import PakistanData from './components/PakistanData';
+import './App.css';
+
+function App() {
+  return (
+    <div >
+      <Header />
+      <MainGrid />
+     
+    </div>
+  );
+}
+
+export default App;
